23803485e0bfeb9c271ed0907fae4d88ae4401c4
James Deathe <james.deathe@jadu.co.uk>
2012-05-28 18:21:16 +0000

    Hide the right hand column from Homepages includeing category homepages.
     - The print, email tools are still visible since the style would need updating to add a border to the bottom of the last right hand item.
     - Rationalised the PHP code in these scripts
     
     FB 28187

--- public_html/site/includes/closing.php
+++ public_html/site/includes/closing.php
@@ -3,56 +3,57 @@
 
 <?php
 	if (!isset($indexPage) || !$indexPage) {
-?>	
-	</div>			
+?>
+	</div>
 	<div id="content-additional">
 	
-	
-<?php 
-	if (basename($_SERVER['SCRIPT_NAME']) == 'events_index.php' || basename($_SERVER['SCRIPT_NAME']) == 'events_info.php' || basename($_SERVER['SCRIPT_NAME']) == 'events_categories.php' || basename($_SERVER['SCRIPT_NAME']) == 'events.php' || basename($_SERVER['SCRIPT_NAME']) == 'events_new.php'  || basename($_SERVER['SCRIPT_NAME']) == 'events_thanks.php' ) {
-		 ?>
+<?php
+		$eventScripts = array(
+			'events.php',
+			'events_index.php',
+			'events_categories.php',
+			'events_info.php',
+			'events_new.php',
+			'events_thanks.php'
+		);
+		if (in_array($script, $eventScripts)) {
+?>
 		<div class="clear"></div>
 		<a class="button red" href="<?php print getSiteRootURL() . buildNewEventURL();?>"><span>Submit your event</span></a>
-<?php		include("../includes/calendar.php");
-	}
-?>
-	
 <?php
-				if (sizeof($allPages) > 1) {
-?>
-
-		<!-- No padge -->
-	
-	<?php
-			} 
-	
-				if (sizeof($allPages) > 1) {
-					$pageNumberPrev = $pageNumber -1;
-					$pageNumberNext = $pageNumber +1;
-					$pageTotal = count($allPages);
-	?>
-				<ul id="pagination">
-					<?php { ?>
-					<li><a href="<?php print getSiteRootURL() . buildDocumentsURL($document->id, $categoryID, $pageNumberPrev);?>">
-					&laquo;</a></li><?php } ?>
-					<li>Page <?php print $pageNumber; ?> of <?php print $pageTotal; ?></li>
-					<?php { ?>
-					<li><a href="<?php print getSiteRootURL() . buildDocumentsURL($document->id, $categoryID, $pageNumberNext);?>">
-					&raquo;</a></li><?php } ?>
-				</ul>
-				<div class="clear"></div>
-				
-			
+			include(HOME . 'site/includes/calendar.php');
+		}
 		
-<?php
-	}
-	else {
+		$pageTotal = count($allPages);
+		if ($pageTotal > 1) {
+			$pageNumberPrev = $pageNumber - 1;
+			$pageNumberNext = $pageNumber + 1;
 ?>
-	
-
-<?php } ?>	
-<div class="clear"></div>
-		<?php if (basename($_SERVER['SCRIPT_NAME']) == 'documents.php') { ?>
+		<ul id="pagination">
+<?php 
+			if ($pageNumberPrev > 0) {
+?>
+			<li><a href="<?php print getSiteRootURL() . buildDocumentsURL($document->id, $categoryID, $pageNumberPrev);?>">&laquo;</a></li>
+<?php
+			}
+?>
+			<li>Page <?php print $pageNumber; ?> of <?php print $pageTotal; ?></li>
+<?php 
+			if ($pageNumberNext <= $pageTotal) {
+?>
+			<li><a href="<?php print getSiteRootURL() . buildDocumentsURL($document->id, $categoryID, $pageNumberNext);?>">&raquo;</a></li>
+<?php
+			}
+?>
+		</ul>
+		<div class="clear"></div>
+<?php
+		}
+?>
+		<div class="clear"></div>
+<?php 
+		if ($script == 'documents_info.php' || $script == 'documents.php' && !$showHomepageContent) { 
+?>
 		<?php include(HOME . 'site/includes/right_supplements.php'); ?>
 		<?php include(HOME . "site/includes/related_info.php"); ?>
 		<div id="pageAction">
@@ -62,29 +63,33 @@
 				<li class="comment"><a href="<?php print getSiteRootURL() . buildFeedbackURL(); ?>">Comment on this page</a></li>
 			</ul>
 		</div>
-		<?php  } ?>
+<?php 
+		}
+?>
 	</div>
-<?php  } ?>	
+<?php
+	}
+?>
 </div>
 <?php
-/*  
+/*
 	PRIMARY COLUMN
 	* To show/hide this column, edit site/includes/lib.php
 */
 	$hideColumn = (boolean) hideColumn();
-	
 	if (($script != "documents_info.php" && $hideColumn == false ) || ($script == "documents_info.php" && $pageStructure->id != '2')) {
 		include(HOME . "/site/includes/structure/column.php");
 	}
-?><div class="clear"></div></div><div id="ieFix"></div>
+?>
+<div class="clear"></div></div><div id="ieFix"></div>
 		<?php include(HOME . "site/includes/structure/footer.php"); ?>
 </div>
 
 <!-- #################################### -->
 <?php
-    if (defined('ANALYTICS_INCLUDE') && ANALYTICS_INCLUDE != '') {
-        print ANALYTICS_INCLUDE;
-    }
+	if (defined('ANALYTICS_INCLUDE') && ANALYTICS_INCLUDE != '') {
+		print ANALYTICS_INCLUDE;
+	}
 ?>
 </body>
 </html>
--- public_html/site/includes/opening.php
+++ public_html/site/includes/opening.php
@@ -6,54 +6,54 @@
 	$liveUpdate = getLiveAnnouncement();
 
 	if (basename($_SERVER['SCRIPT_NAME']) == 'index.php' || Jadu_Service_User::getInstance()->isSessionLoggedIn()) {
-    	include_once('websections/JaduTrackedURLs.php');
-    	include_once('websections/JaduTrackedURLResults.php');
-    }
+		include_once('websections/JaduTrackedURLs.php');
+		include_once('websections/JaduTrackedURLResults.php');
+	}
 
-    $trackedURLs = array();
+	$trackedURLs = array();
 
-    if (Jadu_Service_User::getInstance()->isSessionLoggedIn()) {
+	if (Jadu_Service_User::getInstance()->isSessionLoggedIn()) {
 
-    	if (isset($_REQUEST['trackedURLRead'])) {
-    	    $trackedURLResult = getTrackedURLResult(Jadu_Service_User::getInstance()->getSessionUserID(), $_REQUEST['trackedURLID']);
-    	    confirmTrackedURLRead($trackedURLResult->id);
-    	}
-    	elseif (isset($_REQUEST['trackedURLID']) && is_numeric($_REQUEST['trackedURLID'])) {
-    	    $trackedURL = getTrackedURL($_REQUEST['trackedURLID']);
+		if (isset($_REQUEST['trackedURLRead'])) {
+			$trackedURLResult = getTrackedURLResult(Jadu_Service_User::getInstance()->getSessionUserID(), $_REQUEST['trackedURLID']);
+			confirmTrackedURLRead($trackedURLResult->id);
+		}
+		elseif (isset($_REQUEST['trackedURLID']) && is_numeric($_REQUEST['trackedURLID'])) {
+			$trackedURL = getTrackedURL($_REQUEST['trackedURLID']);
 
-    	    $trackedURLResult = getTrackedURLResult(Jadu_Service_User::getInstance()->getSessionUserID(), $trackedURL->id);
+			$trackedURLResult = getTrackedURLResult(Jadu_Service_User::getInstance()->getSessionUserID(), $trackedURL->id);
 
-    	    if ($trackedURLResult->id == -1) {
-                $trackedURLResult->userID = Jadu_Service_User::getInstance()->getSessionUserID();
-                $trackedURLResult->trackedURLID = $trackedURL->id;
-                newTrackedURLResult($trackedURLResult);
-    	    }
-    	    elseif ($trackedURLResult->confirmedRead > 0) {
-    	        unset($trackedURL);
-    	    }
-    	}
-    	
-    	if (basename($_SERVER['SCRIPT_NAME']) == 'index.php') {
-            if (!Jadu_Service_User::getInstance()->isSessionLoggedIn()) {
-    	        $trackedURLs = getAllTrackedURLs(true);
-            }
-            else {
-                $trackedURLs = getAllUnreadTrackedURLsForUser(Jadu_Service_User::getInstance()->getSessionUserID());
-            }
-        }
+			if ($trackedURLResult->id == -1) {
+				$trackedURLResult->userID = Jadu_Service_User::getInstance()->getSessionUserID();
+				$trackedURLResult->trackedURLID = $trackedURL->id;
+				newTrackedURLResult($trackedURLResult);
+			}
+			elseif ($trackedURLResult->confirmedRead > 0) {
+				unset($trackedURL);
+			}
+		}
+		
+		if (basename($_SERVER['SCRIPT_NAME']) == 'index.php') {
+			if (!Jadu_Service_User::getInstance()->isSessionLoggedIn()) {
+				$trackedURLs = getAllTrackedURLs(true);
+			}
+			else {
+				$trackedURLs = getAllUnreadTrackedURLsForUser(Jadu_Service_User::getInstance()->getSessionUserID());
+			}
+		}
 	}
 	$hideColumn = (boolean) hideColumn();
 
 	$script = basename($_SERVER['SCRIPT_NAME']);
 ?>
 <!--[if lt IE 7]> <body class="ie6<?php if (($script != "documents_info.php" && $hideColumn == false ) || ($script == "documents_info.php" && $pageStructure->id != '2')) { print ' threeCol'; } else { print ' twoCol'; } ?>"> <![endif]-->
-<!--[if IE 7]>    <body class="ie7<?php if (($script != "documents_info.php" && $hideColumn == false ) || ($script == "documents_info.php" && $pageStructure->id != '2')) { print ' threeCol'; } else { print ' twoCol'; } ?>"> <![endif]-->
-<!--[if gte IE 8]>    <body class="ie8<?php if (($script != "documents_info.php" && $hideColumn == false ) || ($script == "documents_info.php" && $pageStructure->id != '2')) { print ' threeCol'; } else { print ' twoCol'; } ?>"> <![endif]-->
+<!--[if IE 7]>	<body class="ie7<?php if (($script != "documents_info.php" && $hideColumn == false ) || ($script == "documents_info.php" && $pageStructure->id != '2')) { print ' threeCol'; } else { print ' twoCol'; } ?>"> <![endif]-->
+<!--[if gte IE 8]>	<body class="ie8<?php if (($script != "documents_info.php" && $hideColumn == false ) || ($script == "documents_info.php" && $pageStructure->id != '2')) { print ' threeCol'; } else { print ' twoCol'; } ?>"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <body> <!--<![endif]-->
 
 <div id="wrapper">
 		<?php include(HOME . "site/includes/structure/header.php"); ?>
-						
+
 <?php
 /* ANNOUNCMENTS */
 	if ($liveUpdate->id != '' && $liveUpdate->id != -1) {
@@ -69,47 +69,46 @@
 		}
 ?>
 					</div>
-					
-					
+
 <?php
 	}
 	// tracked url
 	if (isset($trackedURL) && $trackedURL->id != -1) {
 ?>
 	<div id="trackedURL">
-    	<div class="h2">Have you read this page?</div>
-    	<p><?php print encodeHtml($trackedURL->title); ?></p>
-    	<p><?php print $trackedURL->description; ?></p>
-    	<form action="<?php print getSiteRootURL(); ?>/site/" method="post" enctype="multipart/form-data">
-    	    <input type="hidden" name="trackedURLID" value="<?php print (int) $trackedURL->id; ?>" />
-    	    <input type="submit" name="trackedURLRead" value="I have read this page" />
-    	</form>
+		<div class="h2">Have you read this page?</div>
+		<p><?php print encodeHtml($trackedURL->title); ?></p>
+		<p><?php print encodeHtml($trackedURL->description); ?></p>
+		<form action="<?php print getSiteRootURL(); ?>/site/" method="post" enctype="multipart/form-data">
+			<input type="hidden" name="trackedURLID" value="<?php print (int) $trackedURL->id; ?>" />
+			<input type="submit" name="trackedURLRead" value="I have read this page" />
+		</form>
 	</div>
 <?php
 	}
 	elseif (sizeof($trackedURLs) > 0) {
 ?>
 	<div id="trackedURL">
-    	<div class="h2">Have you read these pages?</div>
-    	<ul>
+		<div class="h2">Have you read these pages?</div>
+		<ul>
 <?php
-        foreach ($trackedURLs as $trackedURL) {
-            $trackedURLID = 'trackedURLID=' . $trackedURL->id;
-            if (mb_strpos($trackedURL->url, '?') === false) {
-                $trackedURLID = '?' . $trackedURLID;
-            }
-            else {
-                $trackedURLID = '&' . $trackedURLID;
-            }
+		foreach ($trackedURLs as $trackedURL) {
+			$trackedURLID = 'trackedURLID=' . $trackedURL->id;
+			if (mb_strpos($trackedURL->url, '?') === false) {
+				$trackedURLID = '?' . $trackedURLID;
+			}
+			else {
+				$trackedURLID = '&' . $trackedURLID;
+			}
 ?>
-            <li><a href="<?php print encodeHtml($trackedURL->url . $trackedURLID); ?>"><?php print encodeHtml($trackedURL->urlText); ?></a></li>
+			<li><a href="<?php print encodeHtml($trackedURL->url . $trackedURLID); ?>"><?php print encodeHtml($trackedURL->urlText); ?></a></li>
 <?php
-        }
+		}
 ?>
-    	</ul>
+		</ul>
 	</div>
 <?php
-    }
+	}
 ?>
 <div id="main" class="box-shadow">
 
@@ -118,8 +117,8 @@
 <?php
 /* CONTENT */
 ?>
-				<div id="content" <?php if (basename($_SERVER['SCRIPT_NAME']) != 'documents.php') { ?>class="full"<?php } ?>>
-				<?php
+				<div id="content"<?php print ($script != 'documents.php' && $script != 'documents_info.php' || $showHomepageContent == true) ? ' class="full"' : ''; ?>>
+<?php
 /*  
 	BREADCRUMB
 	* The breadcrumb can be updated by editing site/inclides/structure/breadcrumb.php
@@ -129,78 +128,69 @@
 					<div id="breadcrumb">
 <?php
 		if (!empty($MAST_BREADCRUMB)) {
-?>		
+?>
 						<!-- Breadcrumb --><!-- googleoff:all -->
 						<ul><?php print $MAST_BREADCRUMB; ?></ul>
 						<!-- END Breadcrumb --><!-- googleon:all -->
 <?php
-	    }
-	    else {
+		}
+		else {
 ?>
 						<!-- Breadcrumb --><!-- googleoff:all -->
 						<ul><li class="bc_end"><a href="<?php print getSiteRootURL(); ?>"><em>You are here:</em> Home</a></li></ul>
 						<!-- END Breadcrumb --><!-- googleon:all -->
 <?php
-	    }
+		}
 ?>
 					</div>
 <?php
 	}
-?>
-
-				
-<?php
 	if (!isset($indexPage) || !$indexPage) {
-?>		
-<?php 
-	if (isset($_GET['categoryID']) && is_numeric($_GET['categoryID'])) {
-		// fetch filtered categories for level 1 category only
-		$catPath = $lgclList->getFullPath($_GET['categoryID']);
-		$topCats = $lgclList->getChildCategories($catPath[0]->id);
-		
-		$parents[] = $_GET['categoryID'];
-		$topicCategoryID = $_GET['categoryID'];
-		while ($topicCategoryID != "") {
-			$topParentCategories = $lgclList->getParentCategories($topicCategoryID);
-			$topicCategoryID = $topParentCategories[0]->id;
-			if ($topicCategoryID != "") {
-				$parents[] = $topicCategoryID;
+		if (isset($_GET['categoryID']) && is_numeric($_GET['categoryID'])) {
+			// fetch filtered categories for level 1 category only
+			$catPath = $lgclList->getFullPath($_GET['categoryID']);
+			$topCats = $lgclList->getChildCategories($catPath[0]->id);
+			
+			$parents[] = $_GET['categoryID'];
+			$topicCategoryID = $_GET['categoryID'];
+			while ($topicCategoryID != "") {
+				$topParentCategories = $lgclList->getParentCategories($topicCategoryID);
+				$topicCategoryID = $topParentCategories[0]->id;
+				if ($topicCategoryID != "") {
+					$parents[] = $topicCategoryID;
+				}
 			}
+			$parents = array_reverse($parents);
 		}
-		$parents = array_reverse($parents);
-	}
-	else {
-		$topCats = $lgclList->GetTopLevelCategories();
-	}
-	$topCats = filterCategoriesInUse($topCats, DOCUMENTS_APPLIED_CATEGORIES_TABLE, true);
-	uasort($topCats, "cmp");
+		else {
+			$topCats = $lgclList->GetTopLevelCategories();
+		}
+		$topCats = filterCategoriesInUse($topCats, DOCUMENTS_APPLIED_CATEGORIES_TABLE, true);
+		uasort($topCats, "cmp");
 ?>
-	<div id="header">
+					<div id="header">
 <?php
-	if (isset($catPath[0])) {
+		if (isset($catPath[0])) {
 ?>
-			<img src="/site/images/headers/<?php print $catPath[0]->id; ?>.jpg" alt="<?php print $catPath[0]->name; ?>" />
-			<h1><?php print encodeHtml($MAST_HEADING); ?></h1>	
-			<div class="clear"></div>
-			</div>
-			<div id="content-inner">
+						<img src="/site/images/headers/<?php print $catPath[0]->id; ?>.jpg" alt="<?php print $catPath[0]->name; ?>" />
+						<h1><?php print encodeHtml($MAST_HEADING); ?></h1>
+						<div class="clear"></div>
+					</div>
+					<div id="content-inner">
 
 <?php
+		}
+		else {
+?>
+						<h1><?php print encodeHtml($MAST_HEADING); ?></h1>
+					</div>
+					<div id="content-inner">
+<?php
+		}
 	}
 	else {
 ?>
-			<h1><?php print encodeHtml($MAST_HEADING); ?></h1>	
-			</div>
-			<div id="content-inner">
-<?php
-	}
-?>
-									
-<?php
-	}
-	else {
-?>
-					<h1 class="hidden"><?php print encodeHtml($MAST_HEADING); ?></h1>
+						<h1 class="hidden"><?php print encodeHtml($MAST_HEADING); ?></h1>
 <?php
 	}
 ?>
