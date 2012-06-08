04d7b3797a988b20d137003ba476725ee21e18d1
James Deathe <james.deathe@jadu.co.uk>
2012-06-08 15:21:19 +0000

    Re-instate access keys.
    FB 28474

--- public_html/site/includes/structure/footer.php
+++ public_html/site/includes/structure/footer.php
@@ -1,22 +1,33 @@
 	<!-- googleoff: index -->
 	<div class="clear"></div>
 	<div id="footer" class="box-shadow">
-				<ul id="smallprint">
-					<li>&copy; <?php print date("Y") . ' ' . encodeHtml(METADATA_GENERIC_NAME); ?> </li>
-					<li>Made with XHTML and CSS to WAI-AA. ICRA rated.</li>
-					<li>Powered by Jadu <a href="http://www.jadu.net" title="Jadu Content Management">Content Management.</a></li>
-				</ul>
-				<div id="social">
-				<h4>Follow us on&hellip;</h4>
-					<ul id="networks">
-						<li><a href="<?php print getSiteRootURL(); ?>/facebook"><img src="<?php print getSiteRootURL(); ?>/site/images/social-facebook.png" alt="Facebook" title="Facebook" /> <span>Facebook</span></a></li>
-						<li><a href="<?php print getSiteRootURL(); ?>/twitter"><img src="<?php print getSiteRootURL(); ?>/site/images/social-twitter.png" alt="Twitter" title="Twitter" /> <span>Twitter</span></a></li>
-						<li><a href="<?php print getSiteRootURL(); ?>/flickr"><img src="<?php print getSiteRootURL(); ?>/site/images/social-flickr.png" alt="Flickr" title="Flickr" /> <span>Flickr</span></a></li>
-						<li><a href="<?php print getSiteRootURL(); ?>/youtube"><img src="<?php print getSiteRootURL(); ?>/site/images/social-youtube.png" alt="YouTube" title="YouTube" /> <span>YouTube</span></a></li>
-						<li><a href="<?php print getSiteRootURL(); ?>/rss"><img src="<?php print getSiteRootURL(); ?>/site/images/social-rss.png" alt="RSS" title="RSS" /> <span>RSS</span></a></li>
-					</ul>
-				</div>
-				<div class="clear"></div>
-			</div>
-	
+		<ul id="smallprint">
+			<li>&copy; <?php print date("Y") . ' ' . encodeHtml(METADATA_GENERIC_NAME); ?> </li>
+			<li>Made with XHTML and CSS to WAI-AA. ICRA rated.</li>
+			<li>Powered by Jadu <a href="http://www.jadu.net" title="Jadu Content Management">Content Management.</a></li>
+		</ul>
+		<div id="social">
+			<h4>Follow us on&hellip;</h4>
+			<ul id="networks">
+				<li><a href="<?php print getSiteRootURL(); ?>/facebook"><img src="<?php print getSiteRootURL(); ?>/site/images/social-facebook.png" alt="Facebook" title="Facebook" /> <span>Facebook</span></a></li>
+				<li><a href="<?php print getSiteRootURL(); ?>/twitter"><img src="<?php print getSiteRootURL(); ?>/site/images/social-twitter.png" alt="Twitter" title="Twitter" /> <span>Twitter</span></a></li>
+				<li><a href="<?php print getSiteRootURL(); ?>/flickr"><img src="<?php print getSiteRootURL(); ?>/site/images/social-flickr.png" alt="Flickr" title="Flickr" /> <span>Flickr</span></a></li>
+				<li><a href="<?php print getSiteRootURL(); ?>/youtube"><img src="<?php print getSiteRootURL(); ?>/site/images/social-youtube.png" alt="YouTube" title="YouTube" /> <span>YouTube</span></a></li>
+				<li><a href="<?php print getSiteRootURL(); ?>/rss"><img src="<?php print getSiteRootURL(); ?>/site/images/social-rss.png" alt="RSS" title="RSS" /> <span>RSS</span></a></li>
+			</ul>
+		</div>
+		<ul class="hidden">
+			<li><a accesskey="1" href="<?php print getSiteRootURL(); ?>">Homepage</a></li>
+			<li><a accesskey="2" href="<?php print getSiteRootURL() . buildWhatsNewURL();?>" rel="nofollow">What&#39;s new</a></li>
+			<li><a accesskey="3" href="<?php print getSiteRootURL() . buildSiteMapURL();?>" rel="nofollow">Site map</a></li>
+			<li><a accesskey="4" href="<?php print getSiteRootURL() . buildSearchURL();?>" rel="nofollow">Search facility </a></li>
+			<li><a accesskey="5" href="<?php print getSiteRootURL() . buildFAQURL();?>" rel="nofollow">Frequently asked questions</a></li>
+			<li><a accesskey="6" href="<?php print getSiteRootURL() . buildAToZURL();?>" rel="nofollow">Help</a></li>
+			<li><a accesskey="7" href="<?php print getSiteRootURL() . buildContactURL();?>" rel="nofollow">Complaints procedure (Contacting the Council page)</a></li>
+			<li><a accesskey="8" href="<?php print getSiteRootURL() . buildTermsURL();?>" rel="nofollow">Terms &amp; Privacy</a></li>
+			<li><a accesskey="9" href="<?php print getSiteRootURL() . buildFeedbackURL();?>" rel="nofollow">Feedback</a></li>
+			<li><a accesskey="0" href="<?php print getSiteRootURL() . buildAccessibilityURL();?>" rel="nofollow">Access key details</a></li>
+		</ul>
+		<div class="clear"></div>
+	</div>
 	<!-- googleon: index -->
