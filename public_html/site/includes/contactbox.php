0567992dfd642cda46067feae1b5b99af9523b5e
Jon Dodd <jon.dodd@jadu.co.uk>
2012-06-14 10:40:58 +0000

    FB28557: Changing content of contact us box.

--- public_html/site/includes/contactbox.php
+++ public_html/site/includes/contactbox.php
@@ -5,9 +5,6 @@
 ?>
 
 <div class="contact">
-	<h4>Contact us</h4>
-	<ul>
-		<li>Email: <a href="mailto:<?php print encodeHtml($address->email); ?>" class="email"><?php print encodeHtml($address->email); ?></a></li>
-		<li>Telephone: <span class="tel"><?php print encodeHtml($address->telephone); ?></span></li>
-	</ul>
+	<h4><a href="<?php print getSiteRootURL(); ?>/contactthecouncil">Contact the Council</a></li>
+	</h4>
 </div>
