3a533c2931282a04a4d77530efd0aebae6e9f9cb
James Deathe <james.deathe@jadu.co.uk>
2012-06-18 19:22:34 +0000

    Redirect the readable URL "jobs"  (and all the Jobs front end scripts) to "jobs_northamptonshire"
     - jobs_northamptonshire should target "http://www.everyroadleadstous.co.uk"
    
    FB 28702

--- public_html/site/scripts/recruit_details.php
+++ public_html/site/scripts/recruit_details.php
@@ -1,36 +1,4 @@
-<?php
-	include_once("utilities/JaduStatus.php");
-	include_once("JaduStyles.php");
-	include_once("marketing/JaduUsers.php");
-	include_once("recruitment/JaduRecruitmentCategories.php");
-	include_once("recruitment/JaduRecruitmentJobs.php");
-	include_once("recruitment/JaduRecruitmentJobsDownloads.php");
-	include_once("marketing/JaduAdverts.php");
-	
-	if (!isset($_GET['jobID']) || $_GET['jobID'] < 1) {
-		header('Location: ' . buildJobsURL());
-	}
-	
-	// Default to approved/live
-	$approvedOnly = true;
-	$liveOnly = true;
-	
-	// Check whether an administrator is previewing the page
-	if (isset($_GET['adminID']) && isset($_GET['preview']) && isset($_GET['expire'])) {
-		include_once('utilities/JaduAdministrators.php');
-		$approvedOnly = $liveOnly = !validateAdminPreviewHash(getAdministrator($_GET['adminID']), $_GET['preview'], $_GET['expire']);
-	}
-	
-	$job = getRecruitmentJob($_GET['jobID'], true, $liveOnly, $approvedOnly);
-	if ($job->id > 0) {
-		$cat = getRecruitmentCategory($job->categoryID);
-	}
-	
-	$downloads = getJobDownloads($_GET['jobID']);
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'Current vacancies';
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li><a href="' . getSiteRootURL() . buildJobsURL() . '">Current vacancies</a></li><li><span>'.encodeHtml($job->title).'</span></li>';
-	
-	include("recruit_details.html.php");
+<?php 
+	header('Location: /jobs_northamptonshire');
+	exit;
 ?>
--- public_html/site/scripts/recruit_jobs.php
+++ public_html/site/scripts/recruit_jobs.php
@@ -1,24 +1,4 @@
-<?php
-	include_once("utilities/JaduStatus.php");
-	include_once("JaduStyles.php");
-	include_once("marketing/JaduUsers.php");
-	include_once("recruitment/JaduRecruitmentCategories.php");
-	include_once("recruitment/JaduRecruitmentJobs.php");
-	include_once("marketing/JaduAdverts.php");
-	
-	// Get all the job categories
-	$categories = getAllRecruitmentCategories();
-	$categoryIDs = array();
-	foreach ($categories as $category) {
-		$categoryIDs[] = $category->id;
-	}
-	
-	// Get the jobs
-	$jobs = getAllRecruitmentJobsInCategories($categoryIDs, true, true, true);
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'Current vacancies';
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li><span>Current vacancies</span></li>';
-	
-	include("recruit_jobs.html.php");
+<?php 
+	header('Location: /jobs_northamptonshire');
+	exit;
 ?>
