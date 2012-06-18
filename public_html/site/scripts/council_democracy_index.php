d0342bfdca29d2b25aace3bb9ee6126becab62c2
James Deathe <james.deathe@jadu.co.uk>
2012-06-18 19:06:25 +0000

    Redirect the readable URL "councillors"  (and all the Councillors front end scripts) to "your_councillors"
     - your_councillors should target "http://www.northamptonboroughcouncil.com/councillors/mgMemberIndex.aspx?bcr=1"
    
    FB 28701

--- public_html/site/scripts/council_democracy_index.php
+++ public_html/site/scripts/council_democracy_index.php
@@ -1,41 +1,4 @@
-<?php
-    include_once("utilities/JaduStatus.php");
-	include_once("JaduStyles.php");
-	include_once("egov/JaduEGovCouncillors.php");
-	include_once("egov/JaduEGovParties.php");
-	include_once("egov/JaduEGovWards.php");
-
-	$allCouncillors = getAllCouncillors(true, true);
-	$numberOfCouncillors = sizeof($allCouncillors);
-	
-	$allParties = getAllParties();
-	
-	$all_party_types = array();
-	
-	$party_types = array();
-	
-	foreach ($allParties as $party) {
-		foreach ($COUNCILLOR_TYPES as $type_id=>$type) {
-			if ($type != 0) {
-				if ($party->type == 1) {
-					$councillors = getCouncillorOfTypeForParty($party->id, $type, true, true);
-					if (!empty($councillors)) {
-						$all_party_types[] = array('shadow'=>array($type_id=>$councillors));
-					}		
-				} 
-				else if ($party->type == 2) {
-					$councillors = getCouncillorOfTypeForParty($party->id, $type, true, true);
-					if (!empty($councillors)) {
-						$all_party_types[] = array('governing'=>array($type_id=>$councillors));
-					}					
-				}
-			}
-		}
-	}	
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'Councillors';
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li><span>Councillors</span></li>';
-	
-	include("council_democracy_index.html.php");
+<?php 
+	header('Location: /your_councillors');
+	exit;
 ?>
--- public_html/site/scripts/councillors_info.php
+++ public_html/site/scripts/councillors_info.php
@@ -1,35 +1,4 @@
-<?php
-	include_once("utilities/JaduStatus.php");
-	include_once("JaduStyles.php"); 
-	include_once("egov/JaduEGovCouncillors.php");
-	include_once("egov/JaduEGovWards.php");
-	include_once("egov/JaduEGovParties.php");
-	include_once("../includes/lib.php"); 
-	
-	if (isset($_GET['councillorID'])) {
-		
-		if ($_GET['councillorID'] != -1 && $_GET['councillorID'] != '') {
-			$councillor = getCouncillor($_GET['councillorID'], true, true);
-		
-			if (!is_int($councillor) || $councillor->id != -1) {
-				$ward = getWard($councillor->wardID);
-				$party = getParty($councillor->partyID);
-			}
-		}
-		else {
-			header("Location: ./councillors.php?viewBy=name");
-			exit;
-		}
-	}
-
-	$typeArray = array();
-	if (is_array($COUNCILLOR_TYPES)) {
-		$typeArray = array_flip($COUNCILLOR_TYPES);
-	}
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'Councillor '. $councillor->forename .' '. $councillor->surname;
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li><a href="' . getSiteRootURL() . buildCouncillorsURL() .'">Councillors</a></li><li><span>' .encodeHtml($councillor->forename .' '. $councillor->surname) .'</span></li>';
-	
-	include("councillors_info.html.php");
+<?php 
+	header('Location: /your_councillors');
+	exit;
 ?>
