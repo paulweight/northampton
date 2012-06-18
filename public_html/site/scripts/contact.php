4b0d0199c314ea1d2ff8656842e33b186953437c
James Deathe <james.deathe@jadu.co.uk>
2012-06-18 18:39:09 +0000

    Redirect the readable URL "contact" to "contactus"
    FB 28701

--- public_html/site/scripts/contact.php
+++ public_html/site/scripts/contact.php
@@ -1,14 +1,4 @@
 <?php 
-	include_once("utilities/JaduStatus.php");
-	include_once("JaduStyles.php");
-	include_once("websections/JaduContact.php");
-
-	$address = new Address;
-	$contactsList = getAllContacts();
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'Contact us';
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li><span>Contact us</span></li>';
-
-	include("contact.html.php");
+	header('Location: /contactus');
+	exit;
 ?>
