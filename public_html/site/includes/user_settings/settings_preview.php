6643223147a7924c75d5d88742b5540add686dd4
James Deathe <james.deathe@jadu.co.uk>
2012-06-19 17:30:25 +0000

    Fixed issue with User settings preview not displaying changes in Font or Letter spacing.
    FB 28477

--- public_html/site/includes/user_settings/settings_preview.php
+++ public_html/site/includes/user_settings/settings_preview.php
@@ -50,36 +50,36 @@
 			}
 		}
 		
-		if (isset($_POST["fontChoice"])) {	
-			if ($_POST["fontChoice"] == "times") {
-				$fontFamily = "times, 'times new roman', palatino, 'new century schoolbook', serif";
-			}
-			elseif ($_POST["fontChoice"] == "comicsans") {
-				$fontFamily = "'comic sans', 'comic sans ms', cursive";
-			}
-			elseif ($_POST["fontChoice"] == "courier") {
-				$fontFamily = "courier, 'courier new', monospace";
-			}
-			elseif ($_POST["fontChoice"] == "ariel") {
-				$fontFamily = "arial, helvetica, 'gill sans', sans-serif";
-			}
-			elseif ($_POST["fontChoice"] == "") {
-				$fontFamily = "Verdana, Tahoma, Arial, Helvetica, Sans-Serif";
+		if (isset($_POST["fontChoice"])) {
+			switch($_POST["fontChoice"]) {
+				case "times":
+					$fontFamily = "times, 'times new roman', palatino, 'new century schoolbook', serif";
+					break;
+				case "comicsans":
+					$fontFamily = "'comic sans', 'comic sans ms', cursive";
+					break;
+				case "courier":
+					$fontFamily = "courier, 'courier new', monospace";
+					break;
+				case "ariel":
+				default:
+					$fontFamily = "arial, helvetica, 'gill sans', sans-serif";
 			}
 		}
 		
 		if (isset($_POST["letterSpacing"])) {
-			if ($_POST["letterSpacing"] == "small") {
-				$textSpacing = "0.1em";
-			}
-			elseif ($_POST["letterSpacing"] == "medium") {
-				$textSpacing = "0.2em";
-			}
-			elseif ($_POST["letterSpacing"] == "wide") {
-				$textSpacing = "0.3em";
-			}
-			elseif ($_POST["letterSpacing"] == "") {
-				$textSpacing = "0";
+			switch($_POST["letterSpacing"]) {
+				case "wide":
+					$textSpacing = "0.1em";
+					break;
+				case "wider":
+					$textSpacing = "0.2em";
+					break;
+				case "widest":
+					$textSpacing = "0.3em";
+					break;
+				default:
+					$textSpacing = "0";
 			}
 		}
 		
@@ -96,10 +96,12 @@
 	}
 ?>
 >
-	<h2 style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>;">Preview</h2>
+	<h2 style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-family: <?php print $fontFamily; ?>; letter-spacing: <?php print $textSpacing; ?>;">Preview</h2>
 	<img src="<?php print getStaticContentRootURL(); ?>/site/images/cute.gif" alt="" class="contentimage" />
-	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam vestibulum velit eu nunc. <a style="background:<?php print $backgroundColour; ?>; color:<?php print $linkColour; ?>;" href="<?php print getSiteRootURL(); ?>">Nullam adipiscing</a> condimentum augue. </p>
-	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>;">Praesent tellus velit, ultricies sed, ornare eu, consectetuer sit amet, felis. Sed mollis vestibulum mauris. Nunc a tortor vitae nibh faucibus interdum.</p>
-	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>;">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce vitae eros. Cras interdum.</p>	
-	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>;"><a style="background:<?php print $backgroundColour; ?>; color:<?php print $linkColour; ?>;" href="<?php print getSiteRootURL(); ?>">Vivamus quam nunc</a>, consequat quis, volutpat non, venenatis sed, nisi. In pede. Cras ut nulla. Etiam scelerisque, est at aliquet suscipit, augue ipsum euismod justo, vitae pellentesque nulla erat nec tellus. Sed a dui sit amet pede tempor semper. In hac habitasse platea dictumst.</p>
+	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>; font-family: <?php print $fontFamily; ?>; letter-spacing: <?php print $textSpacing; ?>;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam vestibulum velit eu nunc. <a style="background:<?php print $backgroundColour; ?>; color:<?php print $linkColour; ?>;" href="<?php print getSiteRootURL(); ?>">Nullam adipiscing</a> condimentum augue. </p>
+	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>; font-family: <?php print $fontFamily; ?>; letter-spacing: <?php print $textSpacing; ?>;">Praesent tellus velit, ultricies sed, ornare eu, consectetuer sit amet, felis. Sed mollis vestibulum mauris. Nunc a tortor vitae nibh faucibus interdum.</p>
+	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>; font-family: <?php print $fontFamily; ?>; letter-spacing: <?php print $textSpacing; ?>;">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce vitae eros. Cras interdum.</p>	
+	<p style="background: <?php print $backgroundColour; ?>; color: <?php print $fontColour; ?>; font-size: <?php print $textSize; ?>; font-family: <?php print $fontFamily; ?>; letter-spacing: <?php print $textSpacing; ?>;"><a style="background:<?php print $backgroundColour; ?>; color:<?php print $linkColour; ?>;" href="<?php print getSiteRootURL(); ?>">Vivamus quam nunc</a>, consequat quis, volutpat non, venenatis sed, nisi. In pede. Cras ut nulla. Etiam scelerisque, est at aliquet suscipit, augue ipsum euismod justo, vitae pellentesque nulla erat nec tellus. Sed a dui sit amet pede tempor semper. In hac habitasse platea dictumst.</p>
 </div>
+
+
