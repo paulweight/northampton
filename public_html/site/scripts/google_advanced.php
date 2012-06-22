539cd5d7a78ff0c7ae469ea6f4d2e2e72934b8b9
James Deathe <james.deathe@jadu.co.uk>
2012-06-22 01:40:01 +0000

    Redirect search to the Search Improve service.
    FB FB 28382

--- public_html/site/scripts/google_advanced.php
+++ public_html/site/scripts/google_advanced.php
@@ -1,12 +1,4 @@
 <?php
-	require_once('utilities/JaduStatus.php');
-	require_once('rupa/JaduRupaSearch.php');
-	
-	$collections = getRupaAdvancedSiteSearchCollections();
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'Advanced search';
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li><span>Advanced search</span></li>';
-	
-	include("google_advanced.html.php");
+	header('Location: /improve_search');
+	exit;
 ?>
--- public_html/site/scripts/search_index.php
+++ public_html/site/scripts/search_index.php
@@ -1,12 +1,4 @@
 <?php
-	include_once("utilities/JaduStatus.php");	
-	include_once("JaduStyles.php");  
-
-	include_once("JaduSearch.php");
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'Advanced search';
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li class="bc_end"><span>Advanced search</span></li>';
-	
-	include("search_index.html.php");
+	header('Location: /improve_search');
+	exit;
 ?>
