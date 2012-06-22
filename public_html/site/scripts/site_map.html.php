6a966c2543a78b3ef10296a1f0d39b38a938271d
James Deathe <james.deathe@jadu.co.uk>
2012-06-22 19:16:03 +0000

    Remove links to unused features - pre-golive checks

--- public_html/site/scripts/site_map.html.php
+++ public_html/site/scripts/site_map.html.php
@@ -70,7 +70,6 @@
 				<li><a href="<?php print getSiteRootURL() . buildNewsURL();?>">Latest news</a></li>
 				<li><a href="<?php print getSiteRootURL() . buildNewsArchiveURL();?>">News archive</a></li>
 				<li><a href="<?php print getSiteRootURL() . buildFeedsURL();?>">External news feeds</a></li>
-				<li><a href="<?php print getSiteRootURL() . buildPastPollResultsURL();?>">Past polls</a></li>
 				<li><a href="<?php print getSiteRootURL() . buildWhatsNewURL();?>">What's new on site</a></li>
 			</ul>
 		
@@ -81,51 +80,6 @@
 			foreach ($rootNewsCategories as $index => $rootCat) {
 ?>
 				<li><a href="<?php print getSiteRootURL() . buildNewsURL($rootCat->id);?>"><?php print encodeHtml($rootCat->name);?> news</a></li>
-<?php
-			}
-?>				
-			</ul>
-		
-			<h2>Galleries</h2>
-			<ul class="list icons galleries">	
-				<li><a href="<?php print getSiteRootURL() . buildMultimediaGalleriesURL(); ?>">Galleries by category</a></li>	
-<?php
-			$galleryCatCount = count($rootGalleryCategories)-1;
-			foreach ($rootGalleryCategories as $index => $rootCat) {
-?>
-				<li><a href="<?php print getSiteRootURL() . buildMultimediaGalleriesURL($rootCat->id); ?>"><?php print encodeHtml($rootCat->name);?> galleries</a></li>
-<?php
-			}
-?>			
-			</ul>
-		
-			<h2>Podcasts</h2>
-			<ul class="list icons podcasts">
-				<li><a href="<?php print getSiteRootURL() . buildMultimediaPodcastsURL(); ?>">Podcasts by category</a></li>				
-<?php
-			$podcastCatCount = count($rootPodcastCategories);				
-			foreach ($rootPodcastCategories as $index => $rootCat) {
-?>
-				<li><a href="<?php print getSiteRootURL() . buildMultimediaPodcastsURL($rootCat->id); ?>"><?php print encodeHtml($rootCat->name);?> podcasts</a></li>
-
-<?php
-			}
-?>
-			</ul>
-		
-			<h2>Press releases</h2>
-			<ul class="list icons press">
-				<li><a href="<?php print getSiteRootURL() . buildPressURL();?>">Press releases</a></li>
-				<li><a href="<?php print getSiteRootURL() . buildPressArchiveURL();?>">Press release archive</a></li>
-			</ul>
-		
-			<h2>Press releases by category</h2>
-			<ul class="list icons press">				
-<?php
-			$pressCatCount = count($rootPressCategories);
-			foreach ($rootPressCategories as $index => $rootCat) {
-?>
-				<li><a href="<?php print getSiteRootURL() . buildPressURL($rootCat->id);?>"><?php print encodeHtml($rootCat->name);?> press releases</a></li>
 <?php
 			}
 ?>				
@@ -163,44 +117,6 @@
 				<li><a href="<?php print getSiteRootURL() . buildFAQURL();?>">Common questions</a></li>
 				<li><a href="<?php print getSiteRootURL() . buildFAQURL(true);?>">Ask a question</a></li>
 			</ul>
-		
-
-<?php
-		if (!Jadu_Service_User::getInstance()->isSessionLoggedIn()) {
-?>
-
-		
-			<h2>Registration</h2>
-			<ul class="list icons councillors">
-				<li><a href="<?php print getSecureSiteRootURL() . buildRegisterURL();?>">Registration form</a></li>
-				<li><a href="<?php print getSecureSiteRootURL() . buildForgotPasswordURL();?>">Password reminder</a></li>
-			</ul>
-			
-<?php
-		}
-		else {
-?>
-				
-		
-			<h2>Your details</h2>
-			<ul class="list icons councillors">
-				<li><a href="<?php print getSecureSiteRootURL() . buildUserHomeURL();?>">Your account</a></li>
-				<li><a href="<?php print getSecureSiteRootURL() . buildChangeDetailsURL();?>">Change your details</a></li>
-				<li><a href="<?php print getSecureSiteRootURL() . buildChangePasswordURL();?>">Change your password</a></li>
-				<li><a href="<?php print getSecureSiteRootURL() . buildForgotPasswordURL();?>">Password reminder</a></li>
-			</ul>
-		
-			<h2>Your online forms</h2>
-			<ul class="list icons forms">
-				<li><a href="<?php print getSecureSiteRootURL() . buildUserHomeURL();?>">Online forms home</a></li>
-				<li><a href="<?php print getSecureSiteRootURL() . buildUserFormURL();?>">Form archive</a></li>
-			</ul>
-		
-<?php
-		}
-?>
-
-	
 
 <!-- ################ MAIN STRUCTURE ############ -->
 <?php include("../includes/closing.php"); ?>
