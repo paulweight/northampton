354d73ece5399ad885f94068c7e4dcb91914d2e0
James Deathe <james.deathe@jadu.co.uk>
2012-07-02 10:04:26 +0000

    Disable External Links script as per customer request
    FB 29068

--- public_html/site/scripts/links.php
+++ public_html/site/scripts/links.php
@@ -1,20 +1,3 @@
 <?php 
-	if (isset($_GET['selectLinks'])) {
-		header("Location: links.php#link".$_GET['selectLinks']);
-		exit();
-	}
-
-	include_once("utilities/JaduStatus.php");
-	include_once("JaduStyles.php");
-	include_once("websections/JaduLinks.php");
-	
-	$categoriesList = getAllLinkCategories();
-	
-	$linksList = getAllLinks();	
-	
-	// Breadcrumb, H1 and Title
-	$MAST_HEADING = 'External links and resources';
-	$MAST_BREADCRUMB = '<li><a href="' . getSiteRootURL() .'" rel="home">Home</a></li><li><span>External links and resources</span></li>';
-	
-	include("links.html.php");
+	include_once('../../404.php');
 ?>
